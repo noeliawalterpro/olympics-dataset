b161447d3e960dd79bd526ca2cd13711b2b1e1e4
noeliawalterpro <noeliawalterpro@gmail.com>
2024-10-01 00:36:38 -0500

    task5

--- task5.sql
+++ task5.sql
@@ -1,8 +1,9 @@
 -- Which nation has participated
 -- in all of the olympic games?
-WITH t1 AS (SELECT `team`, COUNT(DISTINCT `games`) AS CountColum
+WITH t1 AS (SELECT team, COUNT(DISTINCT games) AS CountColum
 FROM athlete_events
-GROUP BY `team`)
-
-SELECT * FROM t1
-WHERE CountColum = 51;
+GROUP BY team),
+t2 AS (SELECT COUNT(DISTINCT games) AS Games_Held FROM athlete_events)
+SELECT t1.team, t1.CountColum
+FROM t1, t2
+WHERE t1.CountColum = t2.Games_Held;
