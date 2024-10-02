ab955d59ca1a709078528b1559dd41314d9fbb4b
noeliawalterpro <noeliawalterpro@gmail.com>
2024-10-01 23:46:41 -0500

    task13v2

--- task13.sql
+++ task13.sql
@@ -1,10 +1,10 @@
 -- Fetch the top 5 most successful countries in olympics. 
 -- Success is defined by no of medals won.
 
-SELECT DISTINCT `team`, COUNT(medal) AS TotalMedals
+SELECT DISTINCT `NOC`, COUNT(medal) AS TotalMedals
 FROM athlete_events
 WHERE NOT medal='NA'
-GROUP BY `team`
+GROUP BY `NOC`
 ORDER BY TotalMedals DESC,`name`  
 LIMIT 5;
 -- NOT ONLY 5 BECAUSE THERE IS TIE
