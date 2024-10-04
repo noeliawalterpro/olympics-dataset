bc2cac17b1915bd37429ed3f9dd09345ca363a1d
noeliawalterpro <noeliawalterpro@gmail.com>
2024-10-03 19:24:42 -0500

    task13v2 with noc_regions

--- task13.sql
+++ task13.sql
@@ -1,10 +1,13 @@
 -- Fetch the top 5 most successful countries in olympics. 
 -- Success is defined by no of medals won.
 
-SELECT DISTINCT `NOC`, COUNT(medal) AS TotalMedals
+WITH t1 AS (SELECT DISTINCT noc, COUNT(medal) AS TotalMedals
 FROM athlete_events
 WHERE NOT medal='NA'
-GROUP BY `NOC`
-ORDER BY TotalMedals DESC,`name`  
+GROUP BY noc
+)
+SELECT region, t1.* FROM t1
+JOIN noc_regions 
+ON noc_regions.noc = t1.noc
+ORDER BY TotalMedals DESC
 LIMIT 5;
--- NOT ONLY 5 BECAUSE THERE IS TIE
