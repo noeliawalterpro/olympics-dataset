6a051642b021a1ac2010608f3978f94fe70d7a24
noeliawalterpro <noeliawalterpro@gmail.com>
2024-10-03 19:31:41 -0500

    task15v2 with noc_regions

--- task15.sql
+++ task15.sql
@@ -4,12 +4,16 @@
 -- List down total gold, silver
 -- and broze medals won by each country.
 
-SELECT 
+WITH t1 AS (SELECT 
     `games`,`NOC`,
    COUNT(CASE WHEN medal = 'gold' THEN 1 END) AS gold,
    COUNT(CASE WHEN medal = 'silver' THEN 1 END) AS silver,
    COUNT(CASE WHEN medal = 'bronze' THEN 1 END) AS bronze
 FROM athlete_events
 GROUP BY games, NOC
+)
+SELECT region, t1.* FROM t1 
+JOIN noc_regions 
+ON t1.noc = noc_regions.noc
 ORDER BY games DESC, gold DESC, silver DESC, bronze DESC;
 
